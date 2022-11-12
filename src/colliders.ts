0b9cb6247f79b01fd1b49c2d7ebedce3ab472e5c
Jakub Zieliński <jzielinski47dev@gmail.com>
2022-11-12 13:31:34 +0100

    updated-colliders

--- src/colliders.ts
+++ src/colliders.ts
@@ -9,7 +9,7 @@ export const colliders = {
         { id: 4, left: 0, right: 32, top: canvas.height - 76, bottom: canvas.height - 68 },
         { id: 5, left: 0, right: 136, top: 86, bottom: 90 },
         { id: 6, left: 176, right: 208, top: 86, bottom: 90 },
-        { id: 7, left: 0, right: 28, top: 0, bottom: 90 }
-
+        { id: 7, left: 10, right: 30, top: 58, bottom: 90 },
+        { id: 8, left: canvas.width - 68, right: canvas.width, top: 82, bottom: 96 }
     ]
 }
