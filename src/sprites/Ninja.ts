28f904c7d78b9a673e06707f08ef37e7ac835306
Jakub Zieliński <jzielinski47dev@gmail.com>
2022-11-30 00:06:14 +0100

    ddd

--- src/sprites/Ninja.ts
+++ src/sprites/Ninja.ts
@@ -2,7 +2,8 @@ import { Transform } from "../types/types";
 import { Sprite } from "./Sprite";
 
 export class Ninja {
+    position: { x: number; y: number; };
     constructor(transform: Transform) {
-
+        this.position = transform.position
     }
 }
