4d8e0f1f8d03408691171c60bfd1ee4b94abda89
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-10-22 23:27:37 +0200

    webpack-config-update

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js'
+        filename: '[name]_bundle.js'
     },
     module: {
         rules: [
