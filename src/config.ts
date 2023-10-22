9257f48f437dbcdd0d26b9cb9c8cbf7c784304d1
Jakub Zieliński <jzielinski47dev@gmail.com>
2023-10-22 23:41:36 +0200

    config-update-speed-bug

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 export const config = {
     dev: { currentScene: 0, inDevelopmendMode: false, inColDetectionMode: false, launched: false, paused: false, lastPossibleScene: 0 },
     canvas: { width: 320, height: 176 },
-    physics: { gravityScale: 0.1, velocity: 0.6, jumpHeight: 1.6, climbSpeed: 4, onWaterSpeed: 0.182 },
+    physics: { gravityScale: 0.1, velocity: 1.2, jumpHeight: 1.6, climbSpeed: 4, onWaterSpeed: 0.182 },
     stats: { score: 0, topScore: 0, lives: 5, collectedLanterns: 0, visited: [0] },
 }
 
